37fd69b7909c0fa56d883a04252de19b1f0399a8
Adrian Skrobacz <adik993@gmail.com>
2016-06-22 22:30:04 +0200

    Removed focusing on current location, because it's irritating when you plan your route

--- NodeLogin/NodeLogin/src/main/java/com/amal/nodelogin/MapsActivity.java
+++ NodeLogin/NodeLogin/src/main/java/com/amal/nodelogin/MapsActivity.java
@@ -24,7 +24,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -241,10 +240,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onLocationChanged(Location location) {
-        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-        if (mMap != null) {
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
-        }
+
     }
 
     @Override
